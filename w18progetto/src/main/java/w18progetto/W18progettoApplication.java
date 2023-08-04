2749be82e87f4b8d6c749904ad73d2735dcd2989
Walter Barbieri <walter90@hotmail.it>
2023-08-04 09:33:37 +0200

    Primo Commit

---
+++ w18progetto/src/main/java/w18progetto/W18progettoApplication.java
@@ -0,0 +1,13 @@
+package w18progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W18progettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W18progettoApplication.class, args);
+	}
+
+}
